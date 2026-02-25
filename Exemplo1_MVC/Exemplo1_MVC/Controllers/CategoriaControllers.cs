b5b6ed290850aefad9afa47c7601312660be6e7e
Joane Gama <joanegama403@gmail.com>
2026-02-25 18:58:06 -0300

    inicio do projeto

---
+++ Exemplo1_MVC/Exemplo1_MVC/Controllers/CategoriaControllers.cs
@@ -0,0 +1,32 @@
+﻿using Exemplo1_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Exemplo1_MVC.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        public static IList<Categoria> categorias = new List<Categoria>() {
+            new Categoria() {
+                CategoriaId = 1,
+                Nome = "Vestuario"
+            },
+            new Categoria() {
+                CategoriaId = 2,
+                Nome = "Eletronicos"
+            },
+            new Categoria() {
+                CategoriaId = 3,
+                Nome = "Utilidades Domésticas"
+            },
+        };
+
+        public IActionResult Index()
+
+        {
+            //gera uma view(exibição HTML
+            //com todas as categorias (cat), cl
+            return View(categorias.OrderBy(cat => cat.CategoriaId ));
+        }
+    }
+}
---
+++ Exemplo1_MVC/Exemplo1_MVC/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace Exemplo1_MVC.Models { 
+
+    public class Categoria
+    {
+        public int CategoriaId {get;set; }
+
+        public string Nome {get; set;}
+
+        public ICollection<Produto> Produtos {get; set;}
+    }
+}
