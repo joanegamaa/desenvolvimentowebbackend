a4381697a5932b71df64f388c55995029d23dc85
Joane Gama <joanegama403@gmail.com>
2026-03-17 21:57:00 -0300

    Operações de CRUD com Entity Framework

--- Aula_EF/Aula_EF/Program.cs
+++ Aula_EF/Aula_EF/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseDefaultServiceProvider(options => {
-    options.ValidateScopes = false; // Ativa a validacao de escopo. False evita erro de BD inexistente
+    // options.ValidateScopes = false; // Ativa a validacao de escopo. False evita erro de BD inexistente
 });
 
 // Add services to the container.
@@ -14,7 +14,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<Context>(options =>
     options.UseSqlServer(builder.Configuration["Data:Exemplo_EF_BD26:ConnectionString"],
 
-    //evita que o BD n?o seja criado por problemas de timeout com o servidor
+    //evita que o BD nao seja criado por problemas de timeout com o servidor
     sqlServerOptionsAction: sqlOptions => {
         sqlOptions.EnableRetryOnFailure(
             maxRetryCount: 10,
@@ -38,7 +38,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Produto}/{action=Index}/{id?}");
 
 //SeedData.Initialize(app.Services);
 SeedData.EnsurePopulated(app);
